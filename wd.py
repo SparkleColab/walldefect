7b64d3fa99d5c67a4d466bcfca1335d145e8c023
SparkleColab <sparklecolab@gmail.com>
2025-04-07 16:39:24 +0530

    Update wd.py

--- wd.py
+++ wd.py
@@ -25,7 +25,7 @@ except KeyError:
 st.write(Google_API_KEY)
 # Configure the Generative AI SDK
 try:
-    client = genai.Client(api_key=Google_API_KEY)
+    client = genai.Client(api_key="AIzaSyAV60kxvHGel5ok_LwFLEOB05GaoHin1s8")
 except Exception as e:
     st.error(f"Error configuring Google AI SDK: {e}")
     st.stop()
